501753d5311aa0a23acd7c252c185d1a16a3465d
Justyna Kowalewska <justynajaglowska1@gmail.com>
2023-04-20 10:37:20 +0200

    getInitialTasks and toggleTaskDone changes

--- src/App.js
+++ src/App.js
@@ -6,13 +6,8 @@ import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
 
-const getInitialTasks = () => {
-  const tasksFromLocalStorage = localStorage.getItem("tasks");
-
-  return tasksFromLocalStorage
-    ? JSON.parse(tasksFromLocalStorage)
-    : [];
-};
+const getInitialTasks = () => 
+    JSON.parse(localStorage.getItem("tasks")) || [];
 
 function App() {
   const [hideDone, setHideDone] = useState(false);
@@ -31,13 +26,11 @@ function App() {
   };
 
   const toggleTaskDone = (id) => {
-    setTasks(tasks => tasks.map(task => {
-      if (task.id === id) {
-        return { ...task, done: !task.done };
-      }
-      return task;
-    }))
-  };
+    setTasks((tasks) => tasks.map((task) => task.id === id 
+        ? { ...task, done: !task.done } 
+        : task
+    ));
+};
 
   const setAllDone = () => {
     setTasks(tasks => tasks.map(task => ({
