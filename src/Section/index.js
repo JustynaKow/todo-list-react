dcefb137be8b5b0f08e0566d90686bdf0dffd931
Justyna Kowalewska <justynajaglowska1@gmail.com>
2023-04-27 13:54:48 +0200

    adding styled-components to Section

--- src/Section/index.js
+++ src/Section/index.js
@@ -1,15 +1,17 @@
-import "./style.css";
+import { Container, Header, Title, Body } from "./styled";
 
 const Section = ({ title, body, extraHeaderContent }) => (
-  <section className="section">
-    <header className="section__header">
-      <h2 className="section__title">{title}</h2>
+  <Container>
+    <Header>
+      <Title>
+        {title}
+      </Title>
       {extraHeaderContent}
-    </header>
-    <div className="section__body">
+    </Header>
+    <Body>
       {body}
-    </div>
-  </section>
+    </Body>
+  </Container>
 );
 
 export default Section;
