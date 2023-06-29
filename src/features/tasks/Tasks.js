8940ab801f71fffba1ac15699cebcc6ef96c7039
Justyna Kowalewska <justynajaglowska1@gmail.com>
2023-06-29 10:39:17 +0200

    remove functions from Tasks

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -4,44 +4,19 @@ import Buttons from "./Buttons";
 import Section from "./../../common/Section";
 import Header from "./../../common/Header";
 import Container from "./../../common/Container";
-import { useTasks } from '../../useTasks';
 
 function Tasks() {
-
-  const {
-    tasks,
-    hideDone,
-    toggleHideDone,
-    removeTask,
-    toggleTaskDone,
-    setAllDone,
-    addNewTask,
-  } = useTasks();
-
   return (
     <Container>
       <Header title="Lista Zadań" />
       <Section
         title="Dodaj nowe zadanie"
-        body={<Form addNewTask={addNewTask} />}
+        body={<Form />}
       />
       <Section
         title="Lista zadań"
-        body={
-          <TaskList
-            tasks={tasks}
-            hideDone={hideDone}
-            removeTask={removeTask}
-            toggleTaskDone={toggleTaskDone}
-          />}
-        extraHeaderContent={
-          <Buttons
-            tasks={tasks}
-            hideDone={hideDone}
-            toggleHideDone={toggleHideDone}
-            setAllDone={setAllDone}
-          />
-        }
+        body={<TaskList />}
+        extraHeaderContent={<Buttons />}
       />
     </Container>
   );
