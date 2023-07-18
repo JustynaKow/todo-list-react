37624853198ee929207d99a2fca7edf463859a04
Justyna Kowalewska <justynajaglowska1@gmail.com>
2023-07-18 16:59:01 +0200

    deleted Container, leave only the styles

--- src/features/tasks/TaskPage/index.js
+++ src/features/tasks/TaskPage/index.js
@@ -1,6 +1,6 @@
 import Section from '../../../common/Section';
 import Header from '../../../common/Header';
-import Container from '../../../common/Container';
+import { Container } from '../../../common/Container';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { getTaskById } from '../tasksSlice';
@@ -15,8 +15,7 @@ function TaskPage() {
       <Section
         title={task ? task.content : 'Nie znaleziono zadania'}
         body={
-          !!task &&
-          (
+          !!task && (
             <>
               <strong>Ukończono:</strong> {task.done ? 'tak' : 'nie'}
             </>
